21a8759c4b73f98a39635d7c8100bc37a0e4df5e
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-10-31 13:17:50 +0530

    printing in colour and adding nodemon

--- app.js
+++ app.js
@@ -1,18 +1,5 @@
-const validator = require('validator')
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 const msg = getNotes()
 console.log(msg)
-
-console.log(validator.isURL('example.com'))
-
-// const fs = require('fs')
-
-// //fs.writeFileSync('notes.txt','Hello, My name is Surabhi.')
-
-// fs.appendFileSync('notes.txt',' I live in Dhanbad.')
-
-// const add = require('./utils.js')
-// //const name = 'Surabhi'
-
-// const sum = add(4,2)
-// console.log(sum) 
+console.log(chalk.blue.inverse('Cool!'));
