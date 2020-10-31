688c7ffbaa26b5666ea43c676e51def5e2fd2bbe
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-10-31 16:49:59 +0530

    arguments passing with yargs

--- app.js
+++ app.js
@@ -1,5 +1,59 @@
 const chalk = require('chalk')
+const { argv } = require('yargs')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
-const msg = getNotes()
-console.log(msg)
-console.log(chalk.blue.inverse('Cool!'));
+
+//Customize yargs version
+yargs.version('1.1.0')
+
+//Create yargs add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: '+ argv.title)
+        console.log('Body: '+ argv.body)
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+//Create a list command
+yargs.command({
+    command: 'list',
+    describe: 'list a new note',
+    handler: function () {
+        console.log('listing a note!')
+    }
+})
+
+//Create a read command
+yargs.command({
+    command: 'read',
+    describe: 'read a new note',
+    handler: function () {
+        console.log('reading a new note')
+    }
+})
+
+// console.log(yargs.argv)
+yargs.parse()
