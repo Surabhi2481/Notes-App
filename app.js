9972753bdaf91a58c2a06edc4cf72231a4a21710
Surabhi2481 <surabhibbsr2481@gmail.com>
2020-10-31 12:08:21 +0530

    importing our own files

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const fs = require('fs')
+const getNotes = require('./notes.js')
+const msg = getNotes()
+console.log(msg)
 
-//fs.writeFileSync('notes.txt','Hello, My name is Surabhi.')
 
-fs.appendFileSync('notes.txt',' I live in Dhanbad.')
+
+// const fs = require('fs')
+
+// //fs.writeFileSync('notes.txt','Hello, My name is Surabhi.')
+
+// fs.appendFileSync('notes.txt',' I live in Dhanbad.')
+
+// const add = require('./utils.js')
+// //const name = 'Surabhi'
+
+// const sum = add(4,2)
+// console.log(sum) 
